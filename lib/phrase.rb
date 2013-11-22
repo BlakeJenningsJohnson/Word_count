3201e539a6f71a23006ae55766f84dc01a92ad17
Your Name <blake.jennings@gmail.com>
2013-11-22 10:16:16 -0800

    Made pretty

--- lib/phrase.rb
+++ lib/phrase.rb
@@ -1,18 +1,15 @@
 class Phrase
-
-def initialize(string)
-  @string = string.downcase.split
-end
-
-def word_count
-  @word_hash = {}
-  array = @string
-  array.uniq.each do |elem|
-    @word_hash[elem] = array.count(elem)
+  def initialize(string)
+    @string = string.downcase.split
   end
-  @word_hash
+
+  def word_count
+    @word_hash = {}
+    array = @string
+    array.uniq.each do |elem|
+      @word_hash[elem] = array.count(elem)
+    end
+    @word_hash
+  end
 end
 
-end
-
-bob = Phrase.new("I have a dog")
